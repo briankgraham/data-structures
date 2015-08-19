b095b3b664cafca23b78daa98a8e7bed1b54650d
Hack Reactor Students <accounts@hackreactor.com>
2015-08-19 16:23:08 -0700

    Complete pseudoclassical implementation of Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sizeVar = 0;
+  this.storage = {};
+};
+
+Stack.prototype.size = function() {
+  return this.sizeVar;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.sizeVar] = value;
+  this.sizeVar++;
+};
+
+Stack.prototype.pop = function() {
+  var result;
+  if(this.sizeVar > 0)
+  {
+    result = this.storage[this.sizeVar-1];
+    this.sizeVar--;
+    delete this.storage[this.sizeVar];
+  }
+
+  return result;
 };
 
 
