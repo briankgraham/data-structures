016d7e126a7fb4c4646cca66a035454f28ac6f41
Hack Reactor Students <accounts@hackreactor.com>
2015-08-19 16:16:50 -0700

    Complete prototypal implementation of Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.sizeVar = 0;
+  newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
+stackMethods.size = function () {
+  return this.sizeVar;
+};
+stackMethods.push = function (value) {
+  this.storage[this.sizeVar] = value;
+  this.sizeVar++;
+};
+stackMethods.pop = function () {
+  var result;
+  if (this.sizeVar > 0) {
+    result = this.storage[this.sizeVar-1];
+    delete this.storage[this.sizeVar-1];
+    this.sizeVar--;
+  }
+  return result;
+};
 
 
