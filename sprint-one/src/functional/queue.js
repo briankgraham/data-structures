6e08f206ce534fecb82928a81008ee6620df175e
Hack Reactor Students <accounts@hackreactor.com>
2015-08-19 12:04:13 -0700

    Completed functional implementation for stack and queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,29 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    var newKey = '' + someInstance.size();
+    storage[newKey] = value;
   };
 
   someInstance.dequeue = function(){
+    var smallestKey = Infinity;
+    for(var key in storage)
+    {
+      if (key < smallestKey)
+        smallestKey = key;
+    }
+    var result = storage[smallestKey];
+    delete storage[smallestKey];
+    return result;
   };
 
   someInstance.size = function(){
+    var sizeCounter = 0;
+    for(var key in storage)
+    {
+      sizeCounter++;
+    }
+    return sizeCounter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,29 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    var newKey = '' + someInstance.size();
+    storage[newKey] = value;
   };
 
   someInstance.pop = function(){
+    var largestKey = -1;
+    for(var key in storage)
+    {
+      if (key > largestKey)
+        largestKey = key;
+    }
+    var result = storage[largestKey];
+    delete storage[largestKey];
+    return result;
   };
 
   someInstance.size = function(){
+    var sizeCounter = 0;
+    for(var key in storage)
+    {
+      sizeCounter++;
+    }
+    return sizeCounter;
   };
 
   return someInstance;
