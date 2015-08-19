f30c1cae5e4031401ff03e104ba699d9c4612651
Hack Reactor Students <accounts@hackreactor.com>
2015-08-19 16:11:03 -0700

    Complete prototypal implementation of Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newQueue = Object.create(queueMethods);
+  newQueue.sizeVar = 0;
+  newQueue.storage = {};
+
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return this.sizeVar;
+};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.sizeVar] = value;
+  this.sizeVar++;
+};
+
+queueMethods.dequeue = function() {
+  var result;
+  if(this.sizeVar > 0)
+  {
+    result = this.storage[0];
+    for(var i = 1; i < this.sizeVar; i++)
+    {
+      this.storage[i-1] = this.storage[i];
+    }
+    this.sizeVar--;
+    delete this.storage[this.sizeVar];
+  }
+  return result;
+};
+
 
 
