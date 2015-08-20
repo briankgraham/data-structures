bbb825f3ae44e20ecd09b6f9b54084f7d8cd4681
Hack Reactor Students <accounts@hackreactor.com>
2015-08-19 21:03:07 -0700

    Complete implementation of Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,23 +4,42 @@ var Tree = function(value){
 
   // your code here
   newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-  // your code here
-  newTree.children = null;  // fix me
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var childTree = new Tree(value);
+  if (!this.children) {
+    this.children = [];
+  }   
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target){
+  
+  var recursiveHelper = function (tree) 
+  {
+    if(tree.value === target)
+      return true;
+    
+    if (tree.children === null)
+      return false;
+    
+    for(var i = 0; i < tree.children.length; i++)
+    {
+      if (recursiveHelper(tree.children[i])) {
+        return true;
+      }
+    }
+    
+    return false;
+  }
 
+  return recursiveHelper(this);
 };
 
 
