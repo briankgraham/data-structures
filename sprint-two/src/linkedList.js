f8af8dab7b6c904d6d16b86f186f4319ed30ec84
Hack Reactor Students <accounts@hackreactor.com>
2015-08-22 15:02:14 -0700

    Draft implementation of double-linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,20 +3,40 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+  list.addToHead = function (value) {
+    var newNode = Node(value);
+    newNode.next = list.head;
+    list.head.prev = newNode;
+    list.head = newNode;
+  };
+
+  list.removeTail = function () {
+    var oldTailValue = list.tail.value;
+    var oldTail = list.tail;
+    oldTail.prev.next = null;
+    list.tail = oldTail.prev;
+    delete oldTail;
+    return oldTailValue;
+  };
+
   list.addToTail = function(value){
     var newNode = Node(value);
     if(list.head===null)
     {
       list.head = newNode;
       list.tail = newNode;
+      return;
     }
+
     list.tail.next = newNode;
+    newNode.prev = list.tail;
     list.tail = newNode;
   };
 
   list.removeHead = function(){
     var oldHeadValue = list.head.value;
     var newHead = list.head.next;
+    newHead.prev = null;
     delete list.head;
     list.head = newHead;
     return oldHeadValue;
@@ -26,8 +46,9 @@ var LinkedList = function(){
     var nodePointer = list.head;
     while(nodePointer)
     {
-      if(nodePointer.value === target)
+      if(nodePointer.value === target) {
         return true;
+      }
       nodePointer = nodePointer.next;
     }
     return false;
@@ -41,6 +62,7 @@ var Node = function(value){
 
   node.value = value;
   node.next = null;
+  node.prev = null;
 
   return node;
 };
