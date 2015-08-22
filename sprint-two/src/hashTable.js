8012d0b16a565c992303b8f91e1b15206e572624
Hack Reactor Students <accounts@hackreactor.com>
2015-08-21 23:06:31 -0700

    Complete advanced implementation of HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,15 +3,11 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.tbd = function() {
-
-};
-
 HashTable.prototype.resize = function() {
-  var bucket;
   var newStorage;
   var tuple;
   var newIndex;
+  var newBuckets;
   var limit = this._limit;
 
   var expandThreshold = 0.75;
@@ -28,29 +24,47 @@ HashTable.prototype.resize = function() {
 
   if (tupleCount > expandThreshold*limit) {
     newStorage = LimitedArray(limit * 2);
-
     this._storage.each(function (bucket, index, collection) {
       if (bucket) {
+        newBuckets = [];
+        
+        for (var a = 0; a < limit * 2; a++) {
+          newBuckets.push([]);
+        }
+
         for (var j = 0; j < bucket.length; j++) {  
           tuple = bucket[j];
           newIndex = getIndexBelowMaxForKey(tuple[0], limit * 2);
-          newStorage.set(newIndex, tuple);
+          newBuckets[newIndex].push(tuple);
+        }
+
+        for (var y = 0; y < newBuckets.length; y++) {
+          newStorage.set(y, newBuckets[y]);
         }
       }
     });
     this._limit *= 2;
     this._storage = newStorage;
   }
-
-  else if ((tupleCount < contractThreshold*limit) && tupleCount > 3) {
+  
+  else if ((tupleCount < contractThreshold*limit) && this._limit !== 8) {
     newStorage = LimitedArray(limit / 2);
 
     this._storage.each(function (bucket, index, collection) {
       if (bucket) {
+        newBuckets = [];
+        for (var b = 0; b < limit / 2; b++) {
+          newBuckets.push([]);
+        }
+
         for (var k = 0; k < bucket.length; k++) {
           tuple = bucket[k];
           newIndex = getIndexBelowMaxForKey(tuple[0], limit / 2);
-          newStorage.set(newIndex, tuple);
+          newBuckets[newIndex].push(tuple);
+        }
+
+        for (var z = 0; z < newBuckets.length; z++) {
+          newStorage.set(z, newBuckets[z]);
         }
       }
     });
@@ -95,6 +109,7 @@ HashTable.prototype.retrieve = function(k){
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+  
   if (bucket) {
     for (var j = 0; j < bucket.length; j++) {
         if (bucket[j][0] === k) {
